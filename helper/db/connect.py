c1b14f0e7b6816093860ff7d0882dee93add6919
Clarck <clarck0122@gmail.com>
2018-10-20 22:55:59 +0800

    add pg sql connection

---
+++ helper/db/connect.py
@@ -0,0 +1,12 @@
+
+"""
+Reference
+https://devcenter.heroku.com/articles/heroku-postgresql#connecting-in-python
+"""
+
+import os
+import psycopg2
+
+DATABASE_URL = os.environ['DATABASE_URL']
+
+conn = psycopg2.connect(DATABASE_URL, sslmode='require')
