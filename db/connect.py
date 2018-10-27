f70348f5c35600b15e4264bf80cb1f3e78060ab0
clarck <clarck0122@gmail.com>
2018-10-27 12:45:24 +0800

    class db module

--- db/connect.py
+++ db/connect.py
@@ -9,4 +9,39 @@ import psycopg2
 
 DATABASE_URL = os.environ['DATABASE_URL']
 
-conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+class Heroku_DB():
+
+    def __init__(self):
+        """ Connect to the PostgreSQL database server """
+        try:
+            # read connection parameters
+            # params = config()
+    
+            # connect to the PostgreSQL server
+            print('Connecting to the PostgreSQL database...')
+            # conn = psycopg2.connect(**params)
+            self.conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+    
+            # create a cursor
+            self.cur = self.conn.cursor()
+            
+        # execute a statement
+            print('PostgreSQL database version:')
+            self.cur.execute('SELECT version()')
+    
+            # display the PostgreSQL database server version
+            db_version = self.cur.fetchone()
+            print(db_version)
+        
+        # close the communication with the PostgreSQL
+            self.cur.close()
+        except (Exception, psycopg2.DatabaseError) as error:
+            print(error)
+        finally:
+            if self.conn is not None:
+                self.conn.close()
+                print('Database connection closed.')
+
+if __name__ == "__main__":
+    conn = Heroku_DB()
+    print("close")
---
+++ helper/ptt_class.py
@@ -0,0 +1,158 @@
+from bs4 import BeautifulSoup
+import requests
+
+
+class ptt_craw():
+    def get_page_number(self, content):
+        start_index = content.find('index')
+        end_index = content.find('.html')
+        page_number = content[start_index + 5: end_index]
+        return int(page_number) + 1
+
+
+    def craw_page(self, res, push_rate):
+        soup_ = BeautifulSoup(res.text, 'html.parser')
+        article_seq = []
+        for r_ent in soup_.find_all(class_="r-ent"):
+            try:
+                # 先得到每篇文章的篇url
+                link = r_ent.find('a')['href']
+                if link:
+                    # 確定得到url再去抓 標題 以及 推文數
+                    title = r_ent.find(class_="title").text.strip()
+                    rate = r_ent.find(class_="nrec").text
+                    url = 'https://www.ptt.cc' + link
+                    if rate:
+                        rate = 100 if rate.startswith('爆') else rate
+                        rate = -1 * int(rate[1]) if rate.startswith('X') else rate
+                    else:
+                        rate = 0
+                    # 比對推文數
+                    if int(rate) >= push_rate:
+                        article_seq.append({
+                            'title': title,
+                            'url': url,
+                            'rate': rate,
+                        })
+            except Exception as e:
+                # print('crawPage function error:',r_ent.find(class_="title").text.strip())
+                print('本文已被刪除', e)
+        return article_seq
+
+
+    def crawl_page_gossiping(self, res):
+        soup = BeautifulSoup(res.text, 'html.parser')
+        article_gossiping_seq = []
+        for r_ent in soup.find_all(class_="r-ent"):
+            try:
+                # 先得到每篇文章的篇url
+                link = r_ent.find('a')['href']
+
+                if link:
+                    # 確定得到url再去抓 標題 以及 推文數
+                    title = r_ent.find(class_="title").text.strip()
+                    url_link = 'https://www.ptt.cc' + link
+                    article_gossiping_seq.append({
+                        'url_link': url_link,
+                        'title': title
+                    })
+
+            except Exception as e:
+                # print u'crawPage function error:',r_ent.find(class_="title").text.strip()
+                # print('本文已被刪除')
+                print('delete', e)
+        return article_gossiping_seq
+
+
+    def ptt_gossiping(self, requests):
+        rs = requests.session()
+        load = {
+            'from': '/bbs/Gossiping/index.html',
+            'yes': 'yes'
+        }
+        res = rs.post('https://www.ptt.cc/ask/over18', verify=False, data=load)
+        soup = BeautifulSoup(res.text, 'html.parser')
+        all_page_url = soup.select('.btn.wide')[1]['href']
+        start_page = self.get_page_number(all_page_url)
+        index_list = []
+        article_gossiping = []
+        for page in range(start_page, start_page - 2, -1):
+            page_url = 'https://www.ptt.cc/bbs/Gossiping/index{}.html'.format(page)
+            index_list.append(page_url)
+
+        # 抓取 文章標題 網址 推文數
+        while index_list:
+            index = index_list.pop(0)
+            res = rs.get(index, verify=False)
+            # 如網頁忙線中,則先將網頁加入 index_list 並休息1秒後再連接
+            if res.status_code != 200:
+                index_list.append(index)
+                # print u'error_URL:',index
+                # time.sleep(1)
+            else:
+                article_gossiping = self.crawl_page_gossiping(res)
+                # print u'OK_URL:', index
+                # time.sleep(0.05)
+        content = ''
+        for index, article in enumerate(article_gossiping, 0):
+            if index == 15:
+                return content
+            data = '{}\n{}\n\n'.format(article.get('title', None), article.get('url_link', None))
+            content += data
+        return content
+
+
+    def ptt_beauty(self, requests):
+        rs = requests.session()
+        res = rs.get('https://www.ptt.cc/bbs/Beauty/index.html', verify=False)
+        soup = BeautifulSoup(res.text, 'html.parser')
+        all_page_url = soup.select('.btn.wide')[1]['href']
+        start_page = self.get_page_number(all_page_url)
+        page_term = 2  # crawler count
+        push_rate = 10  # 推文
+        index_list = []
+        article_list = []
+        for page in range(start_page, start_page - page_term, -1):
+            page_url = 'https://www.ptt.cc/bbs/Beauty/index{}.html'.format(page)
+            index_list.append(page_url)
+
+        # 抓取 文章標題 網址 推文數
+        while index_list:
+            index = index_list.pop(0)
+            res = rs.get(index, verify=False)
+            # 如網頁忙線中,則先將網頁加入 index_list 並休息1秒後再連接
+            if res.status_code != 200:
+                index_list.append(index)
+                # print u'error_URL:',index
+                # time.sleep(1)
+            else:
+                article_list = self.craw_page(res, push_rate)
+                # print u'OK_URL:', index
+                # time.sleep(0.05)
+        content = ''
+        for article in article_list:
+            data = '[{} push] {}\n{}\n\n'.format(article.get('rate', None), article.get('title', None),
+                                                article.get('url', None))
+            content += data
+        return content
+
+
+    def ptt_hot(self, requests):
+        target_url = 'http://disp.cc/b/PttHot'
+        print('Start parsing pttHot....')
+        rs = requests.session()
+        res = rs.get(target_url, verify=False)
+        soup = BeautifulSoup(res.text, 'html.parser')
+        content = ""
+        for data in soup.select('#list div.row2 div span.listTitle'):
+            title = data.text
+            link = "http://disp.cc/b/" + data.find('a')['href']
+            if data.find('a')['href'] == "796-59l9":
+                break
+            content += '{}\n{}\n\n'.format(title, link)
+        return content
+
+if __name__ == "__main__" :
+    ptt = ptt_craw()
+    tmp = ptt.ptt_gossiping(requests)
+    print(tmp)
