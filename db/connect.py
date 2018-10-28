4a8b4b6a8feafa10c71000450e648898966f7a29
Clarck <clarck0122@gmail.com>
2018-10-28 11:21:22 +0800

    practice edit python class

--- db/connect.py
+++ db/connect.py
@@ -47,17 +47,17 @@ class Heroku_DB():
         #         self.conn.close()
         #         print('Database connection closed.')
 
-    def execute_SQL(self, SQL):
+    def execute_SQL(self, SQL, args):
         """ query data from the vendors table """
         try:
 
             # conn = psycopg2.connect(**params)
-            self.conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+            # self.conn = psycopg2.connect(DATABASE_URL, sslmode='require')
     
             # create a cursor
             self.cur = self.conn.cursor()
 
-            self.cur.execute(SQL)
+            self.cur.execute(SQL, args)
             print("The number of parts: ", self.cur.rowcount)
             # row = self.cur.fetchone()
             rows = self.cur.fetchall()
@@ -71,6 +71,24 @@ class Heroku_DB():
         except (Exception, psycopg2.DatabaseError) as error:
             print(error)
 
+    def IsExistUser(self, user_id):
+        # rows = self.execute_SQL("SELECT count(*) FROM userinfo WHERE user_id LIKE %s", [ user_id + '%' ])
+        rows = self.execute_SQL("SELECT count(*) FROM userinfo WHERE user_id = %s", [ user_id ])
+
+        if rows == None:
+            return False
+        else:        
+            return rows[0][0] == 1
+
+    def AddUser(self, user_id, display_name, picture_url, status_message):
+
+        try:    
+            # create a cursor
+            self.cur = self.conn.cursor()
+        except (Exception, psycopg2.DatabaseError) as error:
+            print(error)
+        
+
 
 if __name__ == "__main__":
     
@@ -78,17 +96,23 @@ if __name__ == "__main__":
     conn = Heroku_DB()
     # memory_tracker = tracker.SummaryTracker()
     print("test conn finish")
-    rows = conn.execute_SQL("select * from userinfo")
+    rows = conn.execute_SQL("select * from userinfo",[])
     if rows != None:
         print("not null, len(rows)={}".format(len(rows)))
         for row in rows:
             print (row)
 
 
-    rows = conn.execute_SQL("select * from user_test")
-    if rows != None:
-        print("not null, len(rows)={}".format(len(rows)))
-        for row in rows:
-            print (row)
-    print("test query finish")
-    memory_tracker.print_diff()
+    # rows = conn.execute_SQL("select * from user_test")
+    # if rows != None:
+    #     print("not null, len(rows)={}".format(len(rows)))
+    #     for row in rows:
+    #         print (row)
+    # print("test query finish")
+    # memory_tracker.print_diff()
+
+    test1 = conn.IsExistUser("test5566")
+    
+    test2 = conn.IsExistUser("ta")
+
+    print("test1={},test2={}".format(test1, test2))
