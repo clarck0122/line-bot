2b5ee036b4ddfa8cfd67fef33e2d1022167dbde9
clarck <clarck0122@gmail.com>
2019-03-08 01:03:10 +0800

    add function

--- app.py
+++ app.py
@@ -19,6 +19,7 @@ from linebot.models import *
 from helper.ptt_class import ptt_craw
 from db.connect import Heroku_DB
 from imgur.upload import uploader
+from helper.function import *
 
 
 app = Flask(__name__)
@@ -79,112 +80,6 @@ def callback():
     return 'ok'
 
 
-def pattern_mega(text):
-    patterns = [
-        'mega', 'mg', 'mu', 'ＭＥＧＡ', 'ＭＥ', 'ＭＵ',
-        'ｍｅ', 'ｍｕ', 'ｍｅｇａ', 'GD', 'MG', 'google',
-    ]
-    for pattern in patterns:
-        if re.search(pattern, text, re.IGNORECASE):
-            return True
-
-
-def eyny_movie():
-    target_url = 'http://www.eyny.com/forum-205-1.html'
-    print('Start parsing eynyMovie....')
-    rs = requests.session()
-    res = rs.get(target_url, verify=False)
-    soup = BeautifulSoup(res.text, 'html.parser')
-    content = ''
-    for titleURL in soup.select('.bm_c tbody .xst'):
-        if pattern_mega(titleURL.text):
-            title = titleURL.text
-            if '11379780-1-3' in titleURL['href']:
-                continue
-            link = 'http://www.eyny.com/' + titleURL['href']
-            data = '{}\n{}\n\n'.format(title, link)
-            content += data
-    return content
-
-
-def apple_news():
-    target_url = 'https://tw.appledaily.com/new/realtime'
-    print('Start parsing appleNews....')
-    rs = requests.session()
-    res = rs.get(target_url, verify=False)
-    soup = BeautifulSoup(res.text, 'html.parser')
-    content = ""
-    for index, data in enumerate(soup.select('.rtddt a'), 0):
-        if index == 5:
-            return content
-        link = data['href']
-        content += '{}\n\n'.format(link)
-    return content
-
-
-
-
-
-def movie():
-    target_url = 'http://www.atmovies.com.tw/movie/next/0/'
-    print('Start parsing movie ...')
-    rs = requests.session()
-    res = rs.get(target_url, verify=False)
-    res.encoding = 'utf-8'
-    soup = BeautifulSoup(res.text, 'html.parser')
-    content = ""
-    for index, data in enumerate(soup.select('ul.filmNextListAll a')):
-        if index == 20:
-            return content
-        title = data.text.replace('\t', '').replace('\r', '')
-        link = "http://www.atmovies.com.tw" + data['href']
-        content += '{}\n{}\n'.format(title, link)
-    return content
-
-
-def technews():
-    target_url = 'https://technews.tw/'
-    print('Start parsing movie ...')
-    rs = requests.session()
-    res = rs.get(target_url, verify=False)
-    res.encoding = 'utf-8'
-    soup = BeautifulSoup(res.text, 'html.parser')
-    content = ""
-
-    for index, data in enumerate(soup.select('article div h1.entry-title a')):
-        if index == 12:
-            return content
-        title = data.text
-        link = data['href']
-        content += '{}\n{}\n\n'.format(title, link)
-    return content
-
-
-def panx():
-    target_url = 'https://panx.asia/'
-    print('Start parsing ptt hot....')
-    rs = requests.session()
-    res = rs.get(target_url, verify=False)
-    soup = BeautifulSoup(res.text, 'html.parser')
-    content = ""
-    for data in soup.select('div.container div.row div.desc_wrap h2 a'):
-        title = data.text
-        link = data['href']
-        content += '{}\n{}\n\n'.format(title, link)
-    return content
-
-
-def oil_price():
-    target_url = 'https://gas.goodlife.tw/'
-    rs = requests.session()
-    res = rs.get(target_url, verify=False)
-    soup = BeautifulSoup(res.text, 'html.parser')
-    title = soup.select('#main')[0].text.replace('\n', '').split('(')[0]
-    gas_price = soup.select('#gas-price')[0].text.replace('\n\n\n', '').replace(' ', '')
-    cpc = soup.select('#cpc')[0].text.replace(' ', '')
-    content = '{}\n{}{}'.format(title, gas_price, cpc)
-    return content
-
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
@@ -230,16 +125,16 @@ def handle_message(event):
         line_bot_api.reply_message(
             event.reply_token, image_message)
         return 0
-    if event.message.text == "隨便來張正妹圖片":
-        image = requests.get(API_Get_Image)
-        url = image.json().get('Url')
-        image_message = ImageSendMessage(
-            original_content_url=url,
-            preview_image_url=url
-        )
-        line_bot_api.reply_message(
-            event.reply_token, image_message)
-        return 0
+    # if event.message.text == "隨便來張正妹圖片":
+        # image = requests.get(API_Get_Image)
+        # url = image.json().get('Url')
+        # image_message = ImageSendMessage(
+        #     original_content_url=url,
+        #     preview_image_url=url
+        # )
+        # line_bot_api.reply_message(
+        #     event.reply_token, image_message)
+        # return 0
     if event.message.text == "近期熱門廢文":
         content = ptt_craw.ptt_hot(requests)
         line_bot_api.reply_message(
